95c968f2656f73d76ddf2cb669e3625ca1a14f45
theonlyJoseph <88393500+theonlyJoseph@users.noreply.github.com>
2023-09-25 11:19:46 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
+// Dom elements
 const jokeEl = document.getElementById("joke");
 const jokeBtn = document.getElementById("joke-btn");
 
-// Functions
-
+// functions
 const generateJoke = async () => {
   try {
-  const response = await fetch("https://api.chucknorris.io/jokes/random");
-    if(!response.ok) {
+    const response = await fetch("https://api.chucknorris.io/jokes/random");
+    if (!response.ok) {
       throw new Error(response.message);
     }
-  const data = response.json();
-  jokeEl.innerHTML = data.value;
-    
+    const data = await response.json();
+    jokeEl.innerHTML = data.value;
   } catch (error) {
-    console.log(error)
-    jokeEl.innerHtml = `Something isn't right!`;
-  };
+    console.log(error);
+    jokeEl.innerHTML = `Something went wrong!`;
+  }
 };
 
-// Event handelers
+// App initiator
 function init() {
-jokeBtn.addEventListener("click", generateJoke);
-document.addEventListener("DOMContentLoaded", generateJoke);
-};
+  jokeBtn.addEventListener("click", generateJoke);
+  document.addEventListener("DOMContentLoaded", generateJoke);
+}
 
 init();
+
